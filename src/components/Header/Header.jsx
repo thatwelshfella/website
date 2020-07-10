95bddb673b0233b617ad25c6a8aab03cecaaae12
Luke Delaney <luke.delaney@nationwide.co.uk>
2020-07-10 11:53:36 +0100

    feat(<Header />): basic mobile header created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import './Header.less';
 
 const Header = props => {
-  return <div className="header-component"> HEADER</div>;
+  return (
+    <div className="header-component">
+      Luke Delaney
+      <FontAwesomeIcon icon="bars" size="lg" />
+    </div>
+  );
 };
 
 Header.propTypes = {};
