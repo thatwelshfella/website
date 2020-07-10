23f01b50e64dd58b3679e7ebb628cd91c1129726
Luke Delaney <luke.delaney@nationwide.co.uk>
2020-07-10 11:01:38 +0100

    feat(FontAwesome): icon library added

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import reducer from './redux/reducer';
+import { Provider } from 'react-redux';
 
+import './fontAwesomeLibrary';
 import './Index.less';
 
 const store = createStore(
