5772e0e99b2a7dcd330463a6ca0f15ed070c9d81
Luke Delaney <luke.delaney@nationwide.co.uk>
2020-07-10 10:54:53 +0100

    feat(<Header />): cmp created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Header.less';
+
+const Header = props => {
+  return <div className="header-component"> HEADER</div>;
+};
+
+Header.propTypes = {};
+
+export default Header;
