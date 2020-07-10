95f35828abb2e6575de4aeb907bee197eb58b0c6
Luke Delaney <luke.delaney@nationwide.co.uk>
2020-07-10 13:34:47 +0100

    feat(<Header />): mobile menu overlay created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -6,11 +6,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Header.less';
 
 const Header = props => {
+  const [showMenu, setShowMenu] = useState(true);
+
+  const toggleMobileMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
-    <div className="header-component">
-      Luke Delaney
-      <FontAwesomeIcon icon="bars" size="lg" />
-    </div>
+    <>
+      <div className="header-component">
+        Luke Delaney
+        <FontAwesomeIcon icon="bars" size="lg" onClick={toggleMobileMenu} />
+      </div>
+      <div className={showMenu ? 'mobile-menu show' : 'mobile-menu hidden'}>
+        hidden content
+      </div>
+    </>
   );
 };
 
